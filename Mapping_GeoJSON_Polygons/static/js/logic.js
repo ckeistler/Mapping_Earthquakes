fe9d3e8dd543ab95afff99ac50c0629eb6cfb3af
ckeistler <88443672+ckeistler@users.noreply.github.com>
2021-11-21 18:47:12 -0600

    updating

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -32,7 +32,7 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://github.com/ckeistler/Mapping_Earthquakes/blob/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+let torontoHoods = "https://raw.githubusercontent.com/ckeistler/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 // let myStyle = {
